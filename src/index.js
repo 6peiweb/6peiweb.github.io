1ed1cb4d9574d3c2c09d5601c615a60abdd5dd60
刘培 <liupei.7@bytedance.com>
2022-12-05 11:15:00 +0800

    feat: add short strategy

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { RouterProvider, Link, createHashRouter } from 'react-router-dom';
 
 import KLine from './pages/k-line';
 import LongRoll from './pages/long-roll';
+import ShortRoll from './pages/short-roll';
 
 import './index.css';
 
@@ -13,12 +14,17 @@ const router = createHashRouter([
     path: '/',
     element: <div style={{ padding: 16, display: 'flex', flexDirection: 'column' }}>
       <Link to="/long-roll">多头滚仓策略</Link>
+      <Link to="/short-roll">空头滚仓策略</Link>
       <Link to="/k-line">K线</Link>
     </div>,
   },
   {
     path: '/long-roll',
     element: <LongRoll />,
+  },
+  {
+    path: '/short-roll',
+    element: <ShortRoll />,
   },
   {
     path: '/k-line',
